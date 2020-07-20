622272ec4cc751805da8fb4b93cf60a0a653ce75
yeqinfu <123456>
2020-07-20 12:04:14 +0800

    保存提交

--- src/main/java/testapplication/foodsafety/PageExploreFlow.java
+++ src/main/java/testapplication/foodsafety/PageExploreFlow.java
@@ -25,11 +25,11 @@ public class PageExploreFlow extends BaseTestFlow {
     public void startTest() {
         super.startTest();
         //执行进入关于产品页面
-        // iRouter.currentToTarget(driver, ActivityEnum.AboutActivity);
+         iRouter.currentToTarget(driver, ActivityEnum.AboutActivity);
         //执行进入设置页面
-        //  iRouter.currentToTarget(driver,ActivityEnum.SettingActivity);
+          iRouter.currentToTarget(driver,ActivityEnum.SettingActivity);
         //执行进入 管理操作-八个菜单
-        // iRouter.currentToTarget(driver,ActivityEnum.ManagerOperateActivity);
+         iRouter.currentToTarget(driver,ActivityEnum.ManagerOperateActivity);
 
         //执行进入食材管理
         iRouter.currentToTarget(driver, ActivityEnum.MaterialManagerActivity);
@@ -38,10 +38,12 @@ public class PageExploreFlow extends BaseTestFlow {
 
         //人员健康
         iRouter.currentToTarget(driver, ActivityEnum.PersonHealthyActivity);
-        //留样管理
-        iRouter.currentToTarget(driver, ActivityEnum.ReservedRecordActivity);
+
         //返回主页
         iRouter.currentToTarget(driver, ActivityEnum.MainActivity);
+        //留样管理
+        iRouter.currentToTarget(driver, ActivityEnum.ReservedRecordActivity);
+        iRouter.currentToTarget(driver,ActivityEnum.AboutActivity);
 
 
 
